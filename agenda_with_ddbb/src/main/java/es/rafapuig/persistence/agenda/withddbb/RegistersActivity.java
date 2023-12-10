5c4b3bcdec77a0a0850d18979e7bd30882424b5c
Rafa Puig <rafapuig.profe@gmail.com>
2023-12-10 01:19:23 +0100

    Agenda DB

--- agenda_with_ddbb/src/main/java/es/rafapuig/persistence/agenda/withddbb/RegistersActivity.java
+++ agenda_with_ddbb/src/main/java/es/rafapuig/persistence/agenda/withddbb/RegistersActivity.java
@@ -45,43 +45,26 @@ public class RegistersActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         // TODO: Close the cursor and the database
-        if (cursor != null) cursor.close();
-        if (db != null && db.isOpen()) db.close();
+
         super.onPause();
     }
 
 
     boolean loadPeopleData() {
         // TODO: Make the query, get the cursor and place it in the first tuple
-        cursor = db.rawQuery("SELECT * FROM person", null);
 
-        boolean result = cursor.moveToFirst();
-        return result;
+        return false; // This is a placeholder, replace with your own code
     }
 
 
     public void onPreviousButtonClickCallback(View view) {
         // TODO: Make the cursor point to the previous person at the cursor and display data from this person.
-        if (cursor.moveToPrevious()) {
-            showCurrentPersonData();
-        } else {
-            if (cursor.isBeforeFirst()) {
-                cursor.moveToFirst();
-            }
-            Toast.makeText(this, "There's no previous register", Toast.LENGTH_SHORT).show();
-        }
+
     }
 
     public void onNextButtonClickCallback(View view) {
         // TODO: Make the cursor point to the next person at the cursor and display data for this person.
-        if (cursor.moveToNext()) {
-            showCurrentPersonData();
-        } else {
-            if (cursor.isAfterLast()) {
-                cursor.moveToLast();
-            }
-            Toast.makeText(this, "There's no next register", Toast.LENGTH_SHORT).show();
-        }
+
     }
 
     public void onBackButtonClickCallback(View view) {
@@ -91,13 +74,6 @@ public class RegistersActivity extends AppCompatActivity {
     void showCurrentPersonData() {
         // TODO: Show in the UI data from the tuple / row pointed by the cursor
 
-        String fullName = cursor.getString(0);
-        int age = cursor.getInt(1);
-        boolean isWorking = cursor.getInt(2) == 1 ? true : false;
-
-        binding.fullNameRegisterTextView.setText(fullName);
-        binding.ageRegisterTextView.setText(Integer.toString(age));
-        binding.isWorkingRegisterTextView.setText(isWorking ? "YES" : "NO");
     }
 
 }
